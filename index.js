9b2ed70eb90989cbf78fec02d992b31223c154f7
Diocleniax789 <julioramdom123@gmail.com>
2025-03-17 19:03:55 -0300

    se ha implementado de forma exitosa la transicion de los contenidos

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const botonPlay = document.getElementById('play-boton');
 const pantallaInicial = document.querySelector('.pantalla-inicial');
+const contenidoPrincipal = document.querySelector('.contenido-principal');
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function mostrarOpcionesDeJuego() {
+	
 	pantallaInicial.classList.add('ocultar-pantalla-inicial');
+	
+	await delay(500);
+
+	contenidoPrincipal.innerHTML = `		
+		<div class="contenido-opciones">
+				<h1 class="titulo-principal">Tic-Tac-Toc-Game</h1>
+				<div class="contenedor-botones-opciones">
+					<button class="boton" id="single-player">Single player</button>
+					<button class="boton" id="multi-player">Multiplayer</button>
+					<button class="boton" id="instrucciones">Instrucciones</button>
+				</div>
+		</div>
+	`;
+
+	const contenidoOpciones = document.querySelector('.contenido-opciones');
+	const botonSinglePlayer = document.getElementById('single-player');
+	const botonMultiPlayer = document.getElementById('multi-player');
+	const botonInstrucciones = document.getElementById('instrucciones');
+	
+	await delay(500);
+	
+	contenidoOpciones.classList.add('mostrar-contenido-opciones');
 }
 
 botonPlay.addEventListener('click', mostrarOpcionesDeJuego);
