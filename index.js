00d509398710ca0a0a84a93f4c1d8d6761fe71d2
Diocleniax789 <julioramdom123@gmail.com>
2025-03-17 19:24:51 -0300

    se ha creado la variable que contiene el codigo html para el tablero

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const contenidoPrincipal = document.querySelector('.contenido-principal');
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+async function generarJuegoSinglePlayer() {
+	contenidoPrincipal.innerHTML = `
+		<div class="tablero">
+			<div class="casilla" id="casilla-1"></div>
+			<div class="casilla" id="casilla-2"></div>
+			<div class="casilla" id="casilla-3"></div>
+			<div class="casilla" id="casilla-4"></div>
+			<div class="casilla" id="casilla-5"></div>
+			<div class="casilla" id="casilla-6"></div>
+			<div class="casilla" id="casilla-7"></div>
+			<div class="casilla" id="casilla-8"></div>
+			<div class="casilla" id="casilla-9"></div>
+		</div>
+	`;	
+
+	await delay(500);
+
+	const tableroJuego = document.querySelector('.tablero');
+
+	tableroJuego.classList.add('mostrar-tablero');
+
+}
+
 async function mostrarOpcionesDeJuego() {
 	
 	pantallaInicial.classList.add('ocultar-pantalla-inicial');
@@ -29,6 +52,8 @@ async function mostrarOpcionesDeJuego() {
 	await delay(500);
 	
 	contenidoOpciones.classList.add('mostrar-contenido-opciones');
+
+	botonSinglePlayer.addEventListener('click', generarJuegoSinglePlayer);
 }
 
 botonPlay.addEventListener('click', mostrarOpcionesDeJuego);
