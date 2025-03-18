ba44e7b343c9ed22e1be8238c1dcb7fadea79c28
Diocleniax789 <julioramdom123@gmail.com>
2025-03-17 21:58:39 -0300

    se ha agregado el boton para cerrar el modal

--- index.js
+++ index.js
@@ -24,8 +24,28 @@ async function generarJuegoSinglePlayer() {
 	const tableroJuego = document.querySelector('.tablero');
 
 	tableroJuego.classList.add('mostrar-tablero');
-
 }
+
+async function generarJuegoMultiPlayer() {
+	contenidoPrincipal.innerHTML = `
+		<div class="tablero">
+			<div class="casilla" id="casilla-1"></div>
+			<div class="casilla" id="casilla-2"></div>
+			<div class="casilla" id="casilla-3"></div>
+			<div class="casilla" id="casilla-4"></div>
+			<div class="casilla" id="casilla-5"></div>
+			<div class="casilla" id="casilla-6"></div>
+			<div class="casilla" id="casilla-7"></div>
+			<div class="casilla" id="casilla-8"></div>
+			<div class="casilla" id="casilla-9"></div>
+		</div>
+	`;	
+
+	await delay(500);
+
+	const tableroJuego = document.querySelector('.tablero');
+
+	tableroJuego.classList.add('mostrar-tablero');	}
 
 async function mostrarOpcionesDeJuego() {
 	
@@ -40,6 +60,20 @@ async function mostrarOpcionesDeJuego() {
 					<button class="boton" id="single-player">Single player</button>
 					<button class="boton" id="multi-player">Multiplayer</button>
 					<button class="boton" id="instrucciones">Instrucciones</button>
+					<div class="ventana-modal">
+						<span class="boton-close" id="boton-cerrar">&times;</span>
+						<div class="texto" id="ventana-instrucciones">
+							<h1>How to play?</h1>
+							<ol class="lista-ordenada">
+								<li>Se juega entre dos jugadores.</li>
+								<li>Un jugador usa el símbolo "X" y el otro "O".</li>
+								<li>Los jugadores se turnan para colocar su símbolo en una casilla vacía del tablero.</li>
+								<li>No se puede colocar un símbolo sobre otro ya existente.</li>
+								<li>Gana el primer jugador que logre alinear tres símbolos seguidos en cualquier dirección.</li>
+								<li>Si todas las casillas se llenan y nadie gana, el juego termina en empate.</li>
+							</ol>
+						</div>
+					</div>
 				</div>
 		</div>
 	`;
@@ -48,12 +82,21 @@ async function mostrarOpcionesDeJuego() {
 	const botonSinglePlayer = document.getElementById('single-player');
 	const botonMultiPlayer = document.getElementById('multi-player');
 	const botonInstrucciones = document.getElementById('instrucciones');
-	
+	const ventanaModalInstrucciones = document.querySelector('.ventana-modal');
+	const cerrarVentanaModal = document.getElementById('boton-cerrar');
+
 	await delay(500);
 	
 	contenidoOpciones.classList.add('mostrar-contenido-opciones');
 
 	botonSinglePlayer.addEventListener('click', generarJuegoSinglePlayer);
+	botonMultiPlayer.addEventListener('click', generarJuegoMultiPlayer);
+	botonInstrucciones.addEventListener('click', () => {
+		ventanaModalInstrucciones.style.display = "flex";
+		ventanaModalInstrucciones.classList.add('mostrar-modal');
+	});
+
+	cerrarVentanaModal.addEventListener('click', () => ventanaModalInstrucciones.style.display = "none");
 }
 
 botonPlay.addEventListener('click', mostrarOpcionesDeJuego);
